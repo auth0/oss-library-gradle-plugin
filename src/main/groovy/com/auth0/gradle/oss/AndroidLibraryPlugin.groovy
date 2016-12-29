dcbd88234d8c37ff99c1f82aeba4667d212bc294
Hernan Zalazar <hernan.zalazar@gmail.com>
2016-12-29 17:48:11 -0300

    Missing import

--- src/main/groovy/com/auth0/gradle/oss/AndroidLibraryPlugin.groovy
+++ src/main/groovy/com/auth0/gradle/oss/AndroidLibraryPlugin.groovy
@@ -12,6 +12,7 @@ import org.gradle.api.Project
 import org.gradle.api.publish.maven.MavenPublication
 import org.gradle.api.tasks.bundling.Jar
 import org.gradle.api.tasks.javadoc.Javadoc
+import org.gradle.testing.jacoco.tasks.JacocoReport
 
 class AndroidLibraryPlugin implements Plugin<Project> {
     @Override
